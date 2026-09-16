2bd401402b2bc717d59c508405e17ed3bd179ca0
kratos12-88 <abayomi.giwa@icloud.com>
2026-09-16 14:11:51 +0100

    Add complaint workflow queries

--- backend/src/main/java/com/mms4/smartcampus/repository/ComplaintRepository.java
+++ backend/src/main/java/com/mms4/smartcampus/repository/ComplaintRepository.java
@@ -1,9 +1,14 @@
 package com.mms4.smartcampus.repository;
 
 import com.mms4.smartcampus.model.Complaint;
+import com.mms4.smartcampus.model.ComplaintCategory;
 import com.mms4.smartcampus.model.ComplaintStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+public interface ComplaintRepository extends JpaRepository<Complaint,Long>{
     long countByStatus(ComplaintStatus status);
+    List<Complaint> findBySubmittedByEmailIgnoreCaseOrderByCreatedAtDesc(String email);
+    List<Complaint> findTop20ByOrderByCreatedAtDesc();
+    List<Complaint> findByCategoryAndLocationIgnoreCase(ComplaintCategory category,String location);
 }
