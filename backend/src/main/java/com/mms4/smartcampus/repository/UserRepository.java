9f92f47cbfe137eee0bfca88ba6123585345340f
kratos12-88 <abayomi.giwa@icloud.com>
2026-09-21 22:01:11 +0100

    Support school scoped staff lookup

--- backend/src/main/java/com/mms4/smartcampus/repository/UserRepository.java
+++ backend/src/main/java/com/mms4/smartcampus/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.mms4.smartcampus.repository;
 import com.mms4.smartcampus.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserAccount,Long>{
     Optional<UserAccount> findByEmailIgnoreCase(String email);
+    List<UserAccount> findBySchoolNameIgnoreCaseAndRoleIn(String schoolName, List<String> roles);
 }
